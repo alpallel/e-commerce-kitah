703f30b3ac402c574685e02b47ed92bd0fd9b71b
Farrel Arvindra <farrel.arvindra@gmail.com>
2025-11-25 16:31:52 +0700

    last

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,12 @@
 import "./index.css";
 import { Link } from "react-router-dom";
 import { useAuth } from "./context/AuthContext.jsx";
+import { useCart } from "./context/CartContext.jsx";
 
 function Navbar() {
   const { user, logout, isAuthenticated } = useAuth();
+  const { getTotalItems } = useCart();
+  const total = getTotalItems();
 
   return (
     <header className="bg-sky-950 flex flex-row fixed w-full items-center z-30 h-16">
@@ -28,7 +31,25 @@ function Navbar() {
           Products
         </Link>
         <Link to="/cart" className="relative hover:text-amber-200">
-          Cart
+          {total > 0 && (
+            <span className="absolute -top-2 -left-3 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+              {total}
+            </span>
+          )}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
         </Link>
       </nav>
 
